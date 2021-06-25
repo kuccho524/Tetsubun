ac7737f3683b8112f2a0e99969872348689e4644
kuccho524 <kucchachachanocha@gmail.com>
2021-06-25 23:48:57 +0000

    Modify route of user registration

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # ゲストユーザ
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get '/users', to: 'devise/registrations#new'
   end
 
   # ユーザのルーティング
