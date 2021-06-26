761a4ee31039ccc03ac60de74b7be3ce96d50313
kuccho524 <kucchachachanocha@gmail.com>
2021-06-26 07:44:51 +0000

    Modify search view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,10 +6,10 @@ class SearchesController < ApplicationController
   # 検索結果を表示
   def search
     @range = params[:range]
-    if @range == "Train"
-      @trains = Train.looks(params[:search]).page(params[:page]).reverse_order.per(7)
-    else
+    if @range == "User"
       @users = User.looks(params[:search]).page(params[:page]).per(7)
+    else
+      @trains = Train.looks(params[:search]).page(params[:page]).reverse_order.per(8)
     end
   end
 end
